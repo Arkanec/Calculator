f669f62e2fb41128a577b353a5ffd38dafa98495
Arkanec <black-angel-eremin@yandex.ru>
2023-11-15 23:06:06 +0300

    Мой коммит

---
+++ src/main/java/pro/sky/Calculator/service/impl/CalculatorServiceImpl.java
@@ -0,0 +1,29 @@
+package pro.sky.Calculator.service.impl;
+
+
+import org.springframework.stereotype.Service;
+import pro.sky.Calculator.service.CalculatorService;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+
+    @Override
+    public String plus(int num1, int num2) {
+        return num1 + " + " + num2 + " = " + (num1 + num2);
+    }
+
+    @Override
+    public String minus(int num1, int num2) {
+        return num1 + " - " + num2 + " = " + (num1 - num2);
+    }
+
+    @Override
+    public String multiply(int num1, int num2) {
+        return num1 + " * " + num2 + " = " + (num1 * num2);
+    }
+
+    @Override
+    public String divide(int num1, int num2) {
+        return num1 + " / " + num2 + " = " + (num1 / num2);
+    }
+}
