eb7f591923e3af2226b988382f914de4dede0eb3
Arkanec <black-angel-eremin@yandex.ru>
2023-11-22 23:33:12 +0300

    Мой коммит

--- src/main/java/pro/sky/Calculator/service/impl/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/Calculator/service/impl/CalculatorServiceImpl.java
@@ -9,21 +9,42 @@ public class CalculatorServiceImpl implements CalculatorService {
 
     @Override
     public String plus(int num1, int num2) {
+        String error = calculatorError(num1, num2);
+        if (error != null) {
+            return error;
+        }
         return num1 + " + " + num2 + " = " + (num1 + num2);
     }
 
     @Override
     public String minus(int num1, int num2) {
+        String error = calculatorError(num1, num2);
+        if (error != null) {
+            return error;
+        }
         return num1 + " - " + num2 + " = " + (num1 - num2);
     }
 
     @Override
     public String multiply(int num1, int num2) {
+        String error = calculatorError(num1, num2);
+        if (error != null) {
+            return error;
+        }
         return num1 + " * " + num2 + " = " + (num1 * num2);
     }
 
     @Override
     public String divide(int num1, int num2) {
+        String error = calculatorError(num1, num2);
+        if (error != null) {
+            return error;
+        }
         return num1 + " / " + num2 + " = " + (num1 / num2);
+    }
+
+    @Override
+    public String calculatorError(int num1, int num2) {
+        return null;
     }
 }
